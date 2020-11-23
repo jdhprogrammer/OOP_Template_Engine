6d66ce68ef00cd43bd1ea7fd0689e8504a5bcc9e
David Harris <jdh.programmer@gmail.com>
2020-11-22 22:55:03 -0600

    did a lot of research. had to change some of my app.js function. should be the final one now. need to work more on the questions.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const questions = require("./lib/Questions");
-const inquirer = require("inquirer");
+const { prompt } = require('inquirer');
 const path = require("path");
-const fs = require("fs");
+const { writeFile } = require('fs');
+const { promisify } = require('util');
 const writeFileAsync = promisify(writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -12,24 +13,42 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+let employees = [];
 
 const promptManager = () => {
     return prompt(questions);
 };
 
 const init = async() => {
-    console.log('Answer the Following Questions to build out your Engineering Team Map');
+    console.log('Use the following prompts to build an Organizational Chart Website for you Engineering Team.');
     try {
-        let answers = {};
-        answers = await promptManager();
-        let employees = [answers];
+        const answers = await inquirer.prompt(questions);
+
+        const { name, id, email, role } = answers;
+
+        //Check employee's roll and create correspending class instance
+        switch (role) {
+            case 'Manager':
+                let manager = new Manager(name, id, email, answers.officeNumber);
+                employees.push(manager);
+                break;
+            case 'Engineer':
+                let engineer = new Engineer(name, id, email, answers.github);
+                employees.push(engineer);
+                break;
+            case 'Intern':
+                let intern = new Intern(name, id, email, answers.school);
+                employees.push(intern);
+        }
+
+        //Prompt the questions again when adding team member is chosen
+        if (answers.moreEmployees) init();
 
         console.log(JSON.stringify(employees, null, '\t'));
 
         const html = render(employees);
 
-        await writeFileAsync('team.html', html);
+        await writeFileAsync(outputPath, html);
 
         console.log('Successfully wrote to team.html');
     } catch (err) {
